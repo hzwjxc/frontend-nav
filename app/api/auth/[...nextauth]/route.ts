4f1723d7947f87203ccf04d74c306c92b64b2e8b
黄志文 <542658397@qq.com>
2026-01-08 20:45:07 +0800

    修复构建bcryptjs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-import { compare } from "bcrypt"
+import { compare } from "bcryptjs"
 import NextAuth, { type NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GitHubProvider from "next-auth/providers/github"
