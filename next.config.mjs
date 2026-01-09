2c6f9bd4bed8e35b83c0e5db3f75157853262482
黄志文 <542658397@qq.com>
2026-01-09 20:49:41 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -17,19 +17,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.githubusercontent.com",
-      },
-      {
-        protocol: "https",
         hostname: "*.github.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.gstatic.com",
       },
       {
         protocol: "https",
